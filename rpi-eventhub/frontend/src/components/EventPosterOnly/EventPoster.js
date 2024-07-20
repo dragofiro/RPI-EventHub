9c77a9ed2d64cac93cf51238a8ed0ba6e3efbac8
leemacaravan <leemacaravan@gmail.com>
2024-07-19 23:04:18 -0400

    Pinterest Grid Update
    Pinterest Grid setup now works with the correct sized poster images, however, other CSS border/ updates on different looks need to be done.

--- rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
+++ rpi-eventhub/frontend/src/components/EventPosterOnly/EventPoster.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './EventPoster.css';
 
 const EventPoster = ({ title, posterSrc, description }) => {
+    const posterRef = useRef(null);
+
+    useEffect(() => {
+        const handleLoad = () => {
+            const gridAutoRows = 10; // The base row height set in your CSS
+            const rowGap = 20; // The gap between rows set in your CSS
+            const poster = posterRef.current;
+            if (poster) {
+                // Calculate the height of the image
+                const imgHeight = poster.querySelector('img').offsetHeight;
+                // Calculate the row span
+                const rowSpan = Math.ceil((imgHeight + rowGap) / (gridAutoRows + rowGap));
+                // Set the style for gridRowEnd
+                poster.style.gridRowEnd = `span ${rowSpan}`;
+            }
+        };
+
+        const img = posterRef.current.querySelector('img');
+        img.addEventListener('load', handleLoad);
+
+        // If the image is already loaded, call handleLoad directly
+        if (img.complete) {
+            handleLoad();
+        }
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            img.removeEventListener('load', handleLoad);
+        };
+    }, [posterSrc]); // Only re-run the effect if posterSrc changes
+
     return (
-        <div className="event-poster-container">
-            <img src={posterSrc} alt={title} className="event-poster-img" onLoad={(e) => {
-                const aspectRatio = e.target.naturalWidth / e.target.naturalHeight;
-                e.target.parentNode.style.maxWidth = `${aspectRatio * 100}vh`; // Set a max-width based on the image's aspect ratio and viewport height
-            }}/>
+        <div className="event-poster-container" ref={posterRef}>
+            <img src={posterSrc} alt={title} className="event-poster-img"/>
             <div className="event-poster-details">
                 <h2 className="event-poster-title">{title}</h2>
                 <p className="event-poster-description">{description}</p>
@@ -15,6 +43,5 @@ const EventPoster = ({ title, posterSrc, description }) => {
         </div>
     );
 };
-
 
 export default EventPoster;
